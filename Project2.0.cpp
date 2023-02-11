663f3715bde75d2d77c2e94740daaddfb7b08a59
刘豪杰 <liuhaojie@liuhaojiedeMacBook-Pro-2.local>
2023-02-11 00:48:56 -0800

    project2.0

---
+++ Project2.0.cpp
@@ -0,0 +1,69 @@
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <ctime>
+
+
+
+using namespace std;
+
+// 创建一个struct， student
+struct student{
+    
+    int age;
+    int grade;
+    string name;
+    float addScore, subScore, mulScore, divScore, finalScore;
+    
+};
+// 能够记录多名同学的成绩
+// 并且，可以单独提出某一个人的信息进行修改
+// 注：只可以改名字，grade还有年龄。 不可以改成绩
+// 对所有同学通过成绩进行排名
+// 五个排名
+// add， sub, mul, div, final
+
+
+// 加法练习
+// 减法练习
+// 乘法
+// 除法
+
+// display 展示选择界面
+// showStats 看某一个学生的成绩
+// showAllStats 看所有学生成绩
+// check 看是否正确
+// exam 考试 *这个不用记入排名
+// isValid 看他输入的选项在不在我们的范围内
+
+// 自己发展一个新功能
+// 不可以太过于简单
+// 逻辑代码 不能少于 10 行
+// cout, cin, int good string... ect.
+// 不可以有空行
+// 单独放在一个function里
+
+
+// 不可以用昨天的代码作为你的base
+
+
+// max() min() 不可以用
+// 记得comment
+
+// 斐波那次函数
+// 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+// fib(){}
+// 计算“国王”给了多少粒稻子？
+
+
+
+int main() {
+    
+    
+
+    return 0;
+}
